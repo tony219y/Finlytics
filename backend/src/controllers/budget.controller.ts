56237b82ba893ff7179d31f9d98a0cbc62eb7d7e
tony219y <aekanan2004@gmail.com>
2025-04-19 15:34:58 +0700

    budget endpoint

--- backend/src/controllers/budget.controller.ts
+++ backend/src/controllers/budget.controller.ts
@@ -1,14 +1,34 @@
-import { Request, Response } from 'express';
-import 'dotenv/config'
+import { Request, response, Response } from 'express';
+import { allBudget, createBudget, updateNewBudget } from '../services/budget.service';
 
-
-
-export const getBudget = async (req: Request, res:Response) => {
-    res.status(200).json({message: "dsaw"})
+export const getBudget = async (req: Request, res: Response) => {
+    try {
+        const { userId } = (req as any).user;
+        const response = await allBudget(parseInt(userId))
+        res.status(200).json(response)
+    } catch (error: any) {
+        res.status(400).json({ message: error.message })
+    }
 }
-export const newBudget = async (req: Request, res:Response) => {
-
+export const newBudget = async (req: Request, res: Response) => {
+    try {
+        const { userId } = (req as any).user;
+        const { category, limit, month_year } = await req.body;
+        await createBudget(parseInt(userId), category, limit, month_year)
+        res.status(201).json({ message: "Create a new budget successfully!" })
+    } catch (error: any) {
+        res.status(400).json({ message: error.message })
+    }
 }
-export const updateBudget = async (req: Request, res:Response) => {
-
+export const updateBudget = async (req: Request, res: Response) => {
+    try {
+        const { userId } = (req as any).user;
+        const { category } = req.params;
+        const month_year = req.query.month_year as string;
+        const { limit } = req.body;
+        const response = await updateNewBudget(parseInt(userId), category, month_year, limit)
+        res.status(200).json({ message: "Updated!", response: response, category, month_year, limit })
+    } catch (error: any) {
+        res.status(400).json({ message: error.message })
+    }
 }
---
+++ backend/src/services/budget.service.ts
@@ -0,0 +1,41 @@
+import { prisma } from "../lib/prisma"
+
+export const allBudget = async (userId: number) => {
+    const response = await prisma.budget.findMany({
+        where: {
+            userId: userId
+        }
+    })
+    return response
+}
+
+export const createBudget = async (userId: number, category: string, limit: number, month_year: string) => {
+    const existingBudget = await prisma.budget.findFirst({
+        where: {
+            AND: [
+                { category: category },
+                { month_year: month_year }
+            ]
+        }
+    })
+    if (existingBudget) throw new Error("You have a duplicate budget!");
+
+    const response = await prisma.budget.create({
+        data: {
+            category: category,
+            limit: limit,
+            month_year: month_year,
+            userId: userId
+        }
+    })
+    return response;
+}
+
+export const updateNewBudget = async (userId: number, category: string, month_year: string, limit: number) => {
+    const response = await prisma.budget.updateMany({
+        where: { userId, category, month_year },
+        data: { limit: limit }
+    })
+    return response;
+}
+
